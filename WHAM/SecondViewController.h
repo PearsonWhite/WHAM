5c465531c8499895c9639aac4cf8a85c641635ee
DrStyles <DrStyles602@gmail.com>
2016-03-05 10:08:23 -0800

    Initial Commit

---
+++ WHAM/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  WHAM
+//
+//  Created by Pearson White on 3/5/16.
+//  Copyright (c) 2016 UNR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
