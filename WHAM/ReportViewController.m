8655ef55c4ddb2bc8ddced311159bde1b97e873d
DrStyles <DrStyles602@gmail.com>
2016-04-25 17:19:02 -0700

    keys for report view

--- WHAM/ReportViewController.m
+++ WHAM/ReportViewController.m
@@ -8,6 +8,7 @@
 
 #import "ReportViewController.h"
 #import "GeneratedReportViewController.h"
+#import "constants.h"
 
 @interface ReportViewController ()
 
@@ -35,7 +36,28 @@ GeneratedType generatetedType;
     
     // make sure we have all the data
     NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
-//    NSArray* neededKeys = [[NSArray alloc] ]
+    NSArray* neededKeys = [[NSArray alloc] initWithObjects:
+                         KEY_HISTORY_HPV,
+                         KEY_HPV_VACCINATED,
+                         KEY_HAD_HISTERECTOMY,
+                         KEY_HISTERECTOMY_FOR_CANCER,
+                         KEY_FAMILY_HISTORY_CANCER,
+                         KEY_MOTHER_CANCER,
+                         KEY_SISTER_CANCER,
+                         KEY_DAUGHTER_CANCER,
+                         KEY_SMOKES,
+                         KEY_BIRTH_DATE,
+                         KEY_LAST_MAMMO_DATE,
+                         KEY_LAST_PAP_DATE,
+                         KEY_ABNORMAL_RESULTS_MAMMO,
+                         KEY_ABNORMAL_RESULTS_PAP,
+                         KEY_HPV_TESTED,
+                         nil];
+    for (NSString* key in neededKeys) {
+        if (![[[defaults dictionaryRepresentation] allKeys] containsObject:key]) {
+            #warning todo: hide generate buttons
+        }
+    }
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
